625c47ba380faead6fa64507d9d75a3d4b75f47c
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-31 23:30:19 +0100

    Added a description to the setting

--- pam_random_option/lua/pam/server/extensions/random_option.lua
+++ pam_random_option/lua/pam/server/extensions/random_option.lua
@@ -2,7 +2,7 @@ local name = "random_option"
 PAM_EXTENSION.name = name
 PAM_EXTENSION.enabled = true
 
-local allow_specials_setting = PAM.setting_namespace:AddChild(name):AddSetting("allow_specials", pacoman.TYPE_BOOLEAN, false)
+local allow_specials_setting = PAM.setting_namespace:AddChild(name):AddSetting("allow_specials", pacoman.TYPE_BOOLEAN, false, "Allow this option to randomly select itself and other special options when it's picked.")
 
 function PAM_EXTENSION:RegisterSpecialOptions()
 	PAM.RegisterOption("random_option", function()
